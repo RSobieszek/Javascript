d797659b026fe574dcf24a502569a859e2becd9b
Robert Sobieszek <rsobieszek89@gmail.com>
2019-04-12 17:20:12 +0200

    Add files via upload

---
+++ CodeWars/Who-likes-it-CodeWars.js
@@ -0,0 +1,27 @@
+function likes(names) {
+  if (names.length === 0){
+    return 'no one likes this';
+  } else if (names.length === 1) {
+    return `${names[0]} likes this`;
+  } else if (names.length === 2) {
+    return `${names[0]} and ${names[1]} like this`;
+  } else if (names.length === 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  } else {
+    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
+}
+
+let test1 = [];
+let test2 = ["Peter"];
+let test3 = ["Jacob", "Alex"];
+let test4 = ["Max", "John", "Mark"];
+let test5 = ["Alex", "Jacob", "Mark", "Max"];
+
+likes(test5);
+
+// likes [] // must be "no one likes this"
+// likes ["Peter"] // must be "Peter likes this"
+// likes ["Jacob", "Alex"] // must be "Jacob and Alex like this"
+// likes ["Max", "John", "Mark"] // must be "Max, John and Mark like this"
+// likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like this"
